7ddbc4a55e38612f79c81c7cd40ed19bf1d07788
jomarx <jmaquinay@gmail.com>
2017-05-31 17:47:18 +0800

    update values

--- ReportProblemKioskPP/AnalogRead/AnalogRead.ino
+++ ReportProblemKioskPP/AnalogRead/AnalogRead.ino
@@ -13,47 +13,60 @@
  */
 
 int potPin = A0;    // select the input pin for the potentiometer
-int ledPin = 5;   // select the pin for the LED
 int potVal = 0;       // variable to store the potValue coming from the sensor
 int mbdc = 0;
 
 void setup() {
-  pinMode(ledPin, OUTPUT);  // declare the ledPin as an OUTPUT
   Serial.begin(9600);
 }
 
 void loop() {
   potVal = analogRead(potPin);    // read the potValue from the sensor
-  digitalWrite(ledPin, HIGH);  // turn the ledPin on
-  delay(50);                  // stop the program for some time
-  digitalWrite(ledPin, LOW);   // turn the ledPin off
   delay(50);                  // stop the program for some time
   Serial.println(potVal);
   Serial.println("\n");
   
-	if (potVal > 100 && potVal < 286) {mbdc=1;}
-	else if (potVal > 297 && potVal < 323) {mbdc=2;}
-	else if (potVal > 334 && potVal < 360) {mbdc=3;}
-	else if (potVal > 371 && potVal < 397) {mbdc=4;}
-	else if (potVal > 408 && potVal < 434) {mbdc=5;}
-	else if (potVal > 445 && potVal < 471) {mbdc=6;}
-	else if (potVal > 482 && potVal < 508) {mbdc=7;}
-	else if (potVal > 519 && potVal < 545) {mbdc=8;}
-	else if (potVal > 556 && potVal < 582) {mbdc=9;}
-	else if (potVal > 593 && potVal < 619) {mbdc=10;}
-	else if (potVal > 630 && potVal < 656) {mbdc=11;}
-	else if (potVal > 667 && potVal < 693) {mbdc=12;}
-	else if (potVal > 704 && potVal < 730) {mbdc=13;}
-	else if (potVal > 741 && potVal < 767) {mbdc=14;}
-	else if (potVal > 778 && potVal < 804) {mbdc=15;}
-	else if (potVal > 815 && potVal < 841) {mbdc=16;}
-	else if (potVal > 852 && potVal < 878) {mbdc=17;}
-	else if (potVal > 889 && potVal < 915) {mbdc=18;}
-	else if (potVal > 926 && potVal < 952) {mbdc=19;}
-	else if (potVal > 963 && potVal < 989) {mbdc=20;}
+	if (potVal > 200 && potVal < 210) {mbdc=1;}
+	else if (potVal > 221 && potVal < 231) {mbdc=2;}
+	else if (potVal > 242 && potVal < 252) {mbdc=3;}
+	else if (potVal > 263 && potVal < 273) {mbdc=4;}
+	else if (potVal > 284 && potVal < 294) {mbdc=5;}
+	else if (potVal > 305 && potVal < 315) {mbdc=6;}
+	else if (potVal > 326 && potVal < 336) {mbdc=7;}
+	else if (potVal > 347 && potVal < 357) {mbdc=8;}
+	else if (potVal > 368 && potVal < 378) {mbdc=9;}
+	else if (potVal > 389 && potVal < 399) {mbdc=10;}
+	else if (potVal > 410 && potVal < 420) {mbdc=11;}
+	else if (potVal > 431 && potVal < 441) {mbdc=12;}
+	else if (potVal > 452 && potVal < 462) {mbdc=13;}
+	else if (potVal > 473 && potVal < 483) {mbdc=14;}
+	else if (potVal > 494 && potVal < 504) {mbdc=15;}
+	else if (potVal > 515 && potVal < 525) {mbdc=16;}
+	else if (potVal > 536 && potVal < 546) {mbdc=17;}
+	else if (potVal > 557 && potVal < 567) {mbdc=18;}
+	else if (potVal > 578 && potVal < 588) {mbdc=19;}
+	else if (potVal > 599 && potVal < 609) {mbdc=20;}
+	else if (potVal > 620 && potVal < 630) {mbdc=21;}
+	else if (potVal > 641 && potVal < 651) {mbdc=22;}
+	else if (potVal > 662 && potVal < 672) {mbdc=23;}
+	else if (potVal > 683 && potVal < 693) {mbdc=24;}
+	else if (potVal > 704 && potVal < 714) {mbdc=25;}
+	else if (potVal > 725 && potVal < 735) {mbdc=26;}
+	else if (potVal > 746 && potVal < 756) {mbdc=27;}
+	else if (potVal > 767 && potVal < 777) {mbdc=28;}
+	else if (potVal > 788 && potVal < 798) {mbdc=29;}
+	else if (potVal > 809 && potVal < 819) {mbdc=30;}
+	else if (potVal > 830 && potVal < 840) {mbdc=31;}
+	else if (potVal > 851 && potVal < 861) {mbdc=32;}
+	else if (potVal > 872 && potVal < 882) {mbdc=33;}
+	else if (potVal > 893 && potVal < 903) {mbdc=34;}
+	else if (potVal > 914 && potVal < 924) {mbdc=35;}
+	else if (potVal > 935 && potVal < 945) {mbdc=36;}
+
 	
 	Serial.print("P");
 	Serial.print(mbdc);
 	Serial.println("\n");
+	yield();
 
 }
