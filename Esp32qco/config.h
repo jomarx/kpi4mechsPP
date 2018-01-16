5344b4f1ebf503252ad3d89fc589f0adbd22e7c1
jomarx <jmaquinay@gmail.com>
2018-01-16 13:00:45 +0800

    QCO Tasker intial

---
+++ Esp32qco/config.h
@@ -0,0 +1,7 @@
+//char ssid[] = "outsourcing1.25s";	//  your network SSID (name)
+//char pass[] = "dbafe12345!!";	// your network password
+//char ssid[] = "HanesSucat"; // your SSID
+//char pass[] = "alabang1"; // your SSID Password
+char ssid[] = "kpi4mech"; // your SSID
+char pass[] = "kpi4mech1"; // your SSID Password
+
